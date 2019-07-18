53b62147dbc48ae182e5893af17301a7c6fbcadb
Sarah Gebauer <xgebi@users.noreply.github.com>
2019-07-18 15:30:38 +0200

    Updates

--- precache-manifest.de420e626793cbfb26f6c5ccbcffb60c.js
+++ precache-manifest.de420e626793cbfb26f6c5ccbcffb60c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "121aab98486652523af98d2979a44932",
+    "revision": "7421812e2908802ce4eda28fdb4336dd",
     "url": "/word-workout/index.html"
   },
   {
-    "revision": "b2b8c8229d30c6eb63bc",
-    "url": "/word-workout/static/css/main.41c2c624.chunk.css"
+    "revision": "e399a5b36d53dfbe3329",
+    "url": "/word-workout/static/css/main.ea5a9af4.chunk.css"
   },
   {
     "revision": "c574167a98cb4d522f08",
     "url": "/word-workout/static/js/2.fe7d0a57.chunk.js"
   },
   {
-    "revision": "b2b8c8229d30c6eb63bc",
-    "url": "/word-workout/static/js/main.4ffccaa1.chunk.js"
+    "revision": "e399a5b36d53dfbe3329",
+    "url": "/word-workout/static/js/main.de6f9888.chunk.js"
   },
   {
     "revision": "97f340d0541d5e9907ee",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/word-workout/precache-manifest.91bf45549d6d9d895f17083a3ebecedd.js"
+  "/word-workout/precache-manifest.de420e626793cbfb26f6c5ccbcffb60c.js"
 );
 
 self.addEventListener('message', (event) => {
